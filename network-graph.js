93ea01e915ec35bea134f03ed7bdcf8a3b2aa8ee
MichaelRoberts214 <m_roberts6@u.pacific.edu>
2015-11-06 15:13:31 -0800

    added circuit nodes to graph

--- network-graph.js
+++ network-graph.js
@@ -46,20 +46,46 @@ var data = { data:
   }]
 };
 
+var circuitCount = 0;
+var nodeArr = [];
+
 // sum up the power per circuit
 var panelSum = {};
 for (var i = 0; i < data.data.length; i++) {
   var name = data.data[i].name;
   panelSum[name] = 0;
   for (var j = 0; j < data.data[i].circuits.length; j++) {
-    panelSum[name] += data.data[i].circuits[j].power;
+    var circuitPower = data.data[i].circuits[j].power;
+    panelSum[name] += circuitPower;
+    circuitCount++;
+
+    // add circuit to node array
+    var d = {
+      cluster: i,
+      r: circuitPower,
+      rw: 999.9
+    }
+    nodeArr.push(d);
   }
+
+  // add panel to node array
+  var d = {
+    cluster: i,
+    r: panelSum[name],
+    rw: 999.9
+  }
+  nodeArr.push(d);
 }
+
+// add circuits to node array
+
 
 var panelCount = data.data.length;
 
+var nodeCount = panelCount + circuitCount;
+
 // Variable n is the total number of nodes.
-var n = panelCount;
+var n = nodeCount;
 // Variable m is the number of distinct clusters
 var m = panelCount;
 
@@ -75,15 +101,20 @@ for (var key in panelSum) {
   panelArray.push({name: key, power: panelSum[key]});
 }
 
-var nodes = d3.range(n).map(function(index) {
-  console.log('safadfdas', index);
-  var i = Math.floor(Math.random() * m); // cluster number
-  var r = panelArray[index].power / 50; // set radius here
-  var name = '';
+var nodes = d3.range(nodeCount).map(function(index) {
+  var i = nodeArr[index].cluster;
+  var r = nodeArr[index].r / 50;
   var d = {cluster: i, radius: r, rw: 999.9};
   if (!clusters[i] || (r > clusters[i].radius)) clusters[i] = d;
   return d;
 });
+
+// also want to map based on circuits
+// assign circuit to appropriate panel cluster
+// set radius that is the panel's power
+// name is circuit name
+// d = asdlfjsal;fsadjfkl;jl
+
 
 // put data into nodes
 // for (var i = 0; i < nodes.length; i++) {
