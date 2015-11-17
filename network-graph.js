ae1bfcc9438d31bfa589c4bab31fce87ac3a9ca9
MichaelRoberts214 <m_roberts6@u.pacific.edu>
2015-11-16 16:40:12 -0800

    set force alpha to 0.2 every 0.25s

--- network-graph.js
+++ network-graph.js
@@ -147,6 +147,8 @@ var force = d3.layout.force()
     .on("tick", tick)
     .start();
 
+setInterval(function(){force.alpha(0.2);}, 250);
+
 // Create SVG containing element for graph nodes.
 var svg = d3.select("svg");
 // var svg = d3.select(el).append('svg')
