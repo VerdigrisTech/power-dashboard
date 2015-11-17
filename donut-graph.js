26f367478b9707e0eb26a0cbc4c62da6aab89264
MichaelRoberts214 <m_roberts6@u.pacific.edu>
2015-11-17 09:53:03 -0800

    create sample pie chart

---
+++ donut-graph.js
@@ -0,0 +1,38 @@
+ var dataset = [
+  { label: 'Abulia', count: 10 }, 
+  { label: 'Betelgeuse', count: 20 },
+  { label: 'Cantaloupe', count: 30 },
+  { label: 'Dijkstra', count: 40 }
+];
+
+var width = 360;
+var height = 360;
+var radius = Math.min(width, height) / 2;
+var donutWidth = 75;                            // NEW
+
+var color = d3.scale.category20b();
+
+var svg = d3.select('#donut-graph')
+  // .append('svg')
+  .attr('width', width)
+  .attr('height', height)
+  .append('g')
+  .attr('transform', 'translate(' + (width / 2) + 
+    ',' + (height / 2) + ')');
+
+var arc = d3.svg.arc()
+  .innerRadius(radius - donutWidth)             // NEW
+  .outerRadius(radius);
+  
+var pie = d3.layout.pie()
+  .value(function(d) { return d.count; })
+  .sort(null);
+
+var path = svg.selectAll('path')
+  .data(pie(dataset))
+  .enter()
+  .append('path')
+  .attr('d', arc)
+  .attr('fill', function(d, i) { 
+    return color(d.data.label);
+  });
