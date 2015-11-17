c9e798c7b79ba66bf232eeab832e6c78a2a6e9ef
MichaelRoberts214 <m_roberts6@u.pacific.edu>
2015-11-17 09:55:49 -0800

    remove overwriting d3 height and width attribute assignments

--- donut-graph.js
+++ donut-graph.js
@@ -5,23 +5,20 @@
   { label: 'Dijkstra', count: 40 }
 ];
 
-var width = 360;
-var height = 360;
+var width = 200;
+var height = 200;
 var radius = Math.min(width, height) / 2;
-var donutWidth = 75;                            // NEW
+var donutWidth = 20;
 
 var color = d3.scale.category20b();
 
 var svg = d3.select('#donut-graph')
-  // .append('svg')
-  .attr('width', width)
-  .attr('height', height)
   .append('g')
   .attr('transform', 'translate(' + (width / 2) + 
     ',' + (height / 2) + ')');
 
 var arc = d3.svg.arc()
-  .innerRadius(radius - donutWidth)             // NEW
+  .innerRadius(radius - donutWidth)
   .outerRadius(radius);
   
 var pie = d3.layout.pie()
